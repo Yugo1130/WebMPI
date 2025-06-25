ea66b4f02ff3b1c5c9d824d7ad6591b64fc29338
root <root@LAPTOP-MOVDUFGL.localdomain>
2025-06-25 11:38:11 +0900

    stub creation

--- runtime/client/wasm/src/sample.cpp
+++ runtime/client/wasm/src/sample.cpp
@@ -4,20 +4,20 @@
 int main(int argc, char *argv[]){
     int rank, size;
     // MPI_Status status;
-    // MPI_Init(&argc, &argv);
-    // MPI_Comm_size(MPI_COMM_WORLD, &size);
-    // MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    // printf("Hello from rank %d\n", rank);
-    printf("hello!");
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    printf("Hello from rank %d\n", rank);
     int a = 100;
-    // if(rank == 0){
-    //     MPI_Send(&a, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-    //     printf("rank 0");
-    // }
-    // if(rank == 1){
-    //     MPI_Recv(&a, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-    //     printf("rank 1");
-    // }
-    // MPI_Finalize();
+    if(rank == 0){
+        MPI_Send(&a, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+        printf("rank 0\n");
+    }
+    if(rank == 1){
+        MPI_Recv(&a, 1, MPI_INT, 0, 0, MPI_COMM_WORLD /* , &status */);
+        printf("rank 1\n");
+    }
+    printf("finish!");
+    MPI_Finalize();
     return 0;
 }
