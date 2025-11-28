cfa9e9d4a4600c5ae1a79c9f6c2145afdd3580e0
Yugo1130 <mi.141130.mi63@gmail.com>
2025-11-28 13:07:55 +0900

    方針変更のため一度コミット

--- runtime/client/src/router/router.js
+++ runtime/client/src/router/router.js
@@ -17,6 +17,8 @@ export const ANY_SRC = -1;
 export const ANY_TAG = -1;
 
 function recvMatches(sendSrc, sendTag, sendCommId, requestQueue) {
+    console.log("recvMatches dataQueue:", dataQueue);
+    console.log("recvMatches requestQueue:", requestQueue);
     for (let i = 0; i < requestQueue.length; i++) {
         const recv = requestQueue[i];
         const srcMatch = (recv.src === ANY_SRC) || (sendSrc === recv.src);
@@ -30,6 +32,8 @@ function recvMatches(sendSrc, sendTag, sendCommId, requestQueue) {
 }
 
 function sendMatches(recvSrc, recvTag, recvCommId, dataQueue) {
+    console.log("sendMatches dataQueue:", dataQueue);
+    console.log("sendMatches requestQueue:", requestQueue);
     for (let i = 0; i < dataQueue.length; i++) {
         const send = dataQueue[i];
         if (send.matched) continue; // 既にマッチ済みの場合はスキップ
@@ -43,10 +47,17 @@ function sendMatches(recvSrc, recvTag, recvCommId, dataQueue) {
     return -1; // マッチなし
 }
 
-function writeToSab(eagerSab, src, tag, commId, payload) {
-    const ctlView = new Int32Array(eagerSab, 0, 1); // control    
-    const lenView = new Int32Array(eagerSab, 4, 1); // length            
-    const metaView = new Int32Array(eagerSab, 8, 6); // src, tag, commId, 予備×3
+function writeToSab(eagerSab, src, tag, commId, payload, requestId = undefined) {
+    // ブロッキング用Worker制御用（EMPTY = 0, FULL = 1）
+    const ctlBlockingWorkerView = new Int32Array(eagerSab, 0, 1);
+    // ノンブロッキング用Worker制御用（EMPTY = 0, FULL = 1）
+    const ctlNonBlockingWorkerView = new Int32Array(eagerSab, 4, 1);
+    // router制御用（EMPTY = 0, FULL = 1）
+    const ctlRouterView = new Int32Array(eagerSab, 8, 1);
+    // ノンブロッキング用RequestId伝達用
+    const requestIdView = new Uint32Array(eagerSab, 12, 1);
+    const lenView = new Int32Array(eagerSab, 16, 1); // length            
+    const metaView = new Int32Array(eagerSab, 20, 3); // src, tag, commId,
     const dataView = new Uint8Array(eagerSab, HEADER_SIZE, PAYLOAD_SIZE); // データ領域
 
     const length = Math.min(payload.byteLength, dataView.byteLength);
@@ -58,8 +69,19 @@ function writeToSab(eagerSab, src, tag, commId, payload) {
     Atomics.store(metaView, 2, commId);
 
     // EMPTY -> FULLにしてworkerを起こす
-    Atomics.store(ctlView, 0, FULL);
-    Atomics.notify(ctlView, 0, 1);
+    // ここで対象のworkerをきちんと起こせるかは不明
+    Atomics.store(ctlBlockingWorkerView, 0, FULL);
+    Atomics.store(ctlNonBlockingWorkerView, 0, FULL);
+    Atomics.store(ctlRouterView, 0, FULL);
+    // requestIdがundefinedの場合はブロッキングrecvなのでrequestIdは不要
+    if(requestId === undefined) {
+        console.log("ブロッキング");
+        Atomics.notify(ctlBlockingWorkerView, 0, 1);
+    } else {
+        console.log("ノンブロッキング requestId:", requestId);
+        Atomics.store(requestIdView, 0, requestId);
+        Atomics.notify(ctlNonBlockingWorkerView, 0); // 全てのノンブロッキングwaitを起こす
+    }
 }
 
 function sendToServerMpiMessage(src, dest, tag, commId, payload) {
@@ -78,25 +100,30 @@ function sendToServerMpiMessage(src, dest, tag, commId, payload) {
 
 async function sendingProcess(src, dest, tag, commId, payload) {
     let index = recvMatches(src, tag, commId, requestQueue);
+    console.log("[sendingProcess] matched data at index:", index);
     if (index >= 0) {
+        const requestId = requestQueue[index].requestId ?? undefined;
         // requestQueueから該当エントリを削除
         requestQueue.splice(index, 1);
 
         // 送信先rankのSABを取得
         const eagerSab = rankToSab[dest];
 
-        const ctlView = new Int32Array(eagerSab, 0, 1); // control
+        // router制御用（EMPTY = 0, FULL = 1）
+        const ctlRouterView = new Int32Array(eagerSab, 8, 1);
 
         // 制御フラグがFULLの場合は待機（非同期処理）
-        // ctlView[0] !== FULL：{async:false, value:"not-equal"} が返る
-        // ctlView[0] === FULL：{async:true, value: Promise<"ok"|"timed-out">} が返る
+        // ctlRouterView[0] !== FULL：{async:false, value:"not-equal"} が返る
+        // ctlRouterView[0] === FULL：{async:true, value: Promise<"ok"|"timed-out">} が返る
         while (true) {
-            const result = Atomics.waitAsync(ctlView, 0, FULL);
-            if (!result.async) break;
-            await result.value;
+            const result = Atomics.waitAsync(ctlRouterView, 0, FULL);
+            if (!result.async) break; // EMPTYであれば抜ける
+            await result.value; // FULLの場合は待機
         }
 
-        writeToSab(eagerSab, src, tag, commId, payload);
+        // console.log("[sendingProcess] 再開, requestId:", requestId);
+
+        writeToSab(eagerSab, src, tag, commId, payload, requestId);
     } else {
         // arraybufferにするべきかもしれない
         dataQueue.push({
@@ -121,28 +148,32 @@ export function sendMpiMessage(src, dest, tag, commId, payload) {
     }
 }
 
-export async function recvMpiMessage(src, dest, tag, commId) {
+export async function recvMpiMessage(src, dest, tag, commId, requestId = undefined) {
     let index = sendMatches(src, tag, commId, dataQueue);
+    console.log("[recvMpiMessage] matched data at index:", index);
     if (index >= 0) {
-        // dataQueueから該当エントリを論理削除
+        // dataQueueから該当エントリを論理削除（ここで消さないのはpayloadのコピーを避けるため） 
         dataQueue[index].matched = true;
 
         // 送信先rankのSABを取得
         const eagerSab = rankToSab[dest];
 
-        const ctlView = new Int32Array(eagerSab, 0, 1); // control
+        // router制御用（EMPTY = 0, FULL = 1）
+        const ctlRouterView = new Int32Array(eagerSab, 8, 1);
 
         // 制御フラグがFULLの場合は待機（非同期処理）
-        // ctlView[0] !== FULL：{async:false, value:"not-equal"} が返る
-        // ctlView[0] === FULL：{async:true, value: Promise<"ok"|"timed-out">} が返る
+        // ctlRouterView[0] !== FULL：{async:false, value:"not-equal"} が返る
+        // ctlRouterView[0] === FULL：{async:true, value: Promise<"ok"|"timed-out">} が返る
         while (true) {
-            const result = Atomics.waitAsync(ctlView, 0, FULL);
-            if (!result.async) break;
+            const result = Atomics.waitAsync(ctlRouterView, 0, FULL);
+            if (!result.async) break; // EMPTYであれば抜ける
             await result.value;
         }
 
+        // console.log("[recvMpiMessage] 再開, requestId:", requestId);
+
         // MPI_ANY_SOURCE, MPI_ANY_TAG対応に対応するため，src, tagは送信元の情報を使用する．
-        writeToSab(eagerSab, dataQueue[index].src, dataQueue[index].tag, commId, dataQueue[index].payload);
+        writeToSab(eagerSab, dataQueue[index].src, dataQueue[index].tag, commId, dataQueue[index].payload, requestId);
 
         // SABに書き込んだ後にdataQueueから削除
         dataQueue.splice(index, 1);
@@ -152,6 +183,7 @@ export async function recvMpiMessage(src, dest, tag, commId) {
             dest,
             tag,
             commId,
+            requestId,
         });
     }
 }
